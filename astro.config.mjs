6201dc291ef1632ab99384d3d639086015d80d08
DhaatuTheGamer <chowdhaat2000@gmail.com>
2026-04-18 02:06:07 +0530

    feat: add GitHub Actions deployment workflow and update site configuration URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,35 +8,35 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+    site: 'https://dhaatrik.github.io',
+    integrations: [mdx(), sitemap()],
 
-  fonts: [
-      {
-          provider: fontProviders.local(),
-          name: 'Atkinson',
-          cssVariable: '--font-atkinson',
-          fallbacks: ['sans-serif'],
-          options: {
-              variants: [
-                  {
-                      src: ['./src/assets/fonts/atkinson-regular.woff'],
-                      weight: 400,
-                      style: 'normal',
-                      display: 'swap',
-                  },
-                  {
-                      src: ['./src/assets/fonts/atkinson-bold.woff'],
-                      weight: 700,
-                      style: 'normal',
-                      display: 'swap',
-                  },
-              ],
-          },
-      },
-	],
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: 'Atkinson',
+            cssVariable: '--font-atkinson',
+            fallbacks: ['sans-serif'],
+            options: {
+                variants: [
+                    {
+                        src: ['./src/assets/fonts/atkinson-regular.woff'],
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                    {
+                        src: ['./src/assets/fonts/atkinson-bold.woff'],
+                        weight: 700,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                ],
+            },
+        },
+    ],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
