e29ea57c6e9fb088b19a21464431f12689807bde
DhaatuTheGamer <chowdhaat2000@gmail.com>
2026-05-10 07:11:43 +0530

    feat: implement homepage with dynamic greeting, hero section, and tech stack display

--- src/data/techStack.ts
+++ src/data/techStack.ts
@@ -30,6 +30,10 @@ export const techGroups: TechGroupsType = {
     ],
     Backend: [
         {
+            name: 'Rust',
+            src: 'https://img.shields.io/badge/rust-%23000000.svg?style=for-the-badge&logo=rust&logoColor=white',
+        },
+        {
             name: 'NodeJS',
             src: 'https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white',
         },
