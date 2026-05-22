5ca4d900f17043165b97e24707d2ae4bdc3635a2
DhaatuTheGamer <chowdhaat2000@gmail.com>
2026-05-23 05:21:17 +0530

    feat: add About page and initialize reading list data structure

---
+++ src/data/readingList.ts
@@ -0,0 +1,58 @@
+export interface Book {
+    title: string;
+    author: string;
+    link: string;
+}
+
+export const recommendedBooks: Book[] = [
+    {
+        title: 'As a Man Thinketh',
+        author: 'James Allen',
+        link: 'https://www.amazon.in/As-Man-Thinketh-James-Allen/dp/9386538172',
+    },
+    {
+        title: 'How to Win Friends and Influence People',
+        author: 'Dale Carnegie',
+        link: 'https://www.amazon.in/How-Win-Friends-Influence-People/dp/0671027034?s=bazaar',
+    },
+    {
+        title: 'Marketing Management',
+        author: 'Philip Kotler',
+        link: 'https://www.amazon.in/Marketing-Management-17th-Philip-Kotler/dp/9367138199/ref=sr_1_1_sspa?crid=3LXWM7OV5BGZC&dib=eyJ2IjoiMSJ9.sLFz-n94M3czJDqJ9PP_32HJu72s7iE60M-ItoJp6vdG_i_luKRHC5qjtvFvbwdbhaDQcAcqREZdFZ0Ed0tq4MvMlFxqTsTgReZYC_jjnP86oxfOhad4BxPUlGEJKdmeDfCW7VZbG3SKTxzuPh-YvA4b-NlscmOi4It-IP8RT0okW55U_hkkSUZ4gnB6BNKsQFpR4PmxOjdhLWS8dOlLIbyIG8aAQeS7EB0glkLcZRw.TSj1dE0pcTX9PnG4fTof_JbQM04mjr9Rv2TPs-ne3zg&dib_tag=se&keywords=marketing+management+by+philip+kotler&qid=1778389155&sprefix=Marketing%2Caps%2C567&sr=8-1-spons&aref=WM64g7r717&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1',
+    },
+    {
+        title: 'Zero to One',
+        author: 'Peter Thiel',
+        link: 'https://www.amazon.in/Zero-One-Notes-Startups-Future/dp/0804139296',
+    },
+    {
+        title: 'The Pragmatic Programmer',
+        author: 'Andrew Hunt & David Thomas',
+        link: 'https://www.amazon.in/Pragmatic-Programmer-journey-mastery-20th/dp/0135957052',
+    },
+    {
+        title: 'Deep Work',
+        author: 'Cal Newport',
+        link: 'https://www.amazon.in/Deep-Work-Rules-Focused-Success/dp/0349411905',
+    },
+    {
+        title: 'Atomic Habits',
+        author: 'James Clear',
+        link: 'https://www.amazon.in/Atomic-Habits-James-Clear/dp/1847941834',
+    },
+    {
+        title: 'Clean Code',
+        author: 'Robert C. Martin',
+        link: 'https://www.amazon.in/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882',
+    },
+    {
+        title: 'The Lean Startup',
+        author: 'Eric Ries',
+        link: 'https://www.amazon.in/The-Lean-Startup-Eric-Ries/dp/0307887898',
+    },
+    {
+        title: 'The Alchemist',
+        author: 'Paulo Coelho',
+        link: 'https://www.amazon.in/Alchemist-Paulo-Coelho/dp/8172234988',
+    },
+];
